c02ed6885adb2621278af87e3a055c6b67d5cb6b
Naor Movshovitz <nmovshov@gmail.com>
2016-06-29 17:06:28 -0700

    adding first useful barotrope - Polytrope.m untested

---
+++ +barotropes/Polytrope.m
@@ -0,0 +1,40 @@
+classdef Polytrope < barotropes.Barotrope
+    %POLYTROPE A barotrope of the form P = K*rho^(1 + 1/n).
+    
+    %% Properties
+    properties (Access = public)
+        K % polytropic constant, dimensions depend on index
+        n % polytropic index, dimensionless
+        alpha % 1 + 1/n
+    end
+    
+    %% The constructor
+    methods
+        function obj = Polytrope(K, n)
+            if nargin > 0
+                assert(isnumeric(K) && isscalar(K) && K > 0)
+                assert(isnumeric(n) && isscalar(n) && n > 0)
+                obj.K = K;
+                obj.n = n;
+                obj.alpha = 1 + 1/n;
+            end
+        end
+    end
+    
+    %% Required barotrope methods
+    methods
+        function test(obj)
+            disp(obj)
+        end
+        
+        function P = pressure(obj,rho)
+            P = obj.K*rho.^(obj.alpha);
+        end
+        
+        function rho = density(obj,P)
+            rho = (P/obj.K).^(1/obj.alpha);
+        end
+    end
+    
+end
+
