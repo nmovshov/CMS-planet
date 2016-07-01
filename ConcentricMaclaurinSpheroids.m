cc4084d23a0afd8ee3a28ef45a96baf4bd282a46
Naor Movshovitz <nmovshov@gmail.com>
2016-07-01 16:45:27 -0700

    interim commit working on cmsset options parser

--- ConcentricMaclaurinSpheroids.m
+++ ConcentricMaclaurinSpheroids.m
@@ -6,25 +6,72 @@ classdef ConcentricMaclaurinSpheroids
     %   (2013).
     
     %% Properties
+    properties (Access = public)
+        opts
+    end
     properties (GetAccess = public, SetAccess = private)
-        nlayers
-        nangles
-        lambda
-        delta
-        zeta
+        lambdas
+        deltas
+        zetas
+        Js
     end
     
     %% The constructor
     methods
-        function obj = ConcentricMaclaurinSpheroids(nlayers, nangles)
-            if nargin > 0
-                obj.nlayers = nlayers;
-                obj.nangles = nangles;
-                obj.lambda = linspace(1, 0, nlayers)';
-                obj.zeta = repmat(obj.lambda, 1, nangles);
+        function obj = ConcentricMaclaurinSpheroids(opts)
+            if nargin == 0
+                opts = ConcentricMaclaurinSpheroids.cmsset();
             end
+            obj.lambdas = linspace(1, 1/opts.nlayers, opts.nlayers)';
+            obj.zetas = repmat(obj.lambdas, 1, opts.nangles);
+            obj.opts = opts;
         end
     end
     
-end
+    %% Ordinary methods
+    methods
+        %function y = foo(obj, x) ...
+    end
+    
+    %% Static methods
+    methods (Static)
+        function options = cmsset(varargin)
+        %CMSSET Create cms options structure.
+        %   OPTIONS = CMSSET('NAME1',VALUE1,'NAME2',VALUE2,...) creates an
+        %   options structure OPTIONS in which the named properties have the
+        %   specified values. Any unspecified properties have default values.
+        %   Case is ignored for property names.
+        %
+        %   CMSSET with no input arguments displays all property names and their
+        %   possible values.
+        %
+        %CMSSET PROPERTIES
+        %
+        %nlayers - Number of constant density layers [positive integer {512}]
+        %
+        %nangles - Number of colatitude points used to define level surfaces [positive integer {48}]
+        %
+        %nmoments - Degree to carry out mulitpole expansion of gravity moments [positive even {12}]
+        %
+        %   Note: defaults chosen to match Hubbard (2013) example.
+        
+            % Print out possible values of properties.
+            if (nargin == 0) && (nargout == 0)
+                fprintf('  nlayers: [positive scalar integer {512}]\n');
+                fprintf('  nangles: [positive scalar integer {48}]\n');
+                fprintf('  nmoments: [positive scalar even {12}]\n');
+                return
+            end
+            
+            % Parse name-value pairs.
+            if rem(nargin, 2) ~= 0
+                error('Name/Value pair mismatch')
+            end
+            %TODO:CONTINUE
+            
+        end
+        
+    end % End of static methods
+    
+end % End of classdef
 
