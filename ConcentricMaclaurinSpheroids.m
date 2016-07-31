ccd1b96348c3a784a7afdf9ef41049fb08dedace
Naor Movshovitz <nmovshov@gmail.com>
2016-07-30 19:09:42 -0700

    removed progressbar for now from everything. tweaked optional comms: lvl 2 verbosity gives separate timings for update_zeta and update_J while lvl 1 (def) gives just sum for each pass in relax() put in gauleg() as a local function in ConcentricMaclaurinSpheroids.m but also leaving a copy as a global function. the idea is i can remove assertions from the local function. i can see how this might be confusing though... have to think about this more.

--- ConcentricMaclaurinSpheroids.m
+++ ConcentricMaclaurinSpheroids.m
@@ -76,9 +76,6 @@ classdef ConcentricMaclaurinSpheroids < handle
             if (verb > 0)
                 fprintf('Relaxing CMS to self-consistent level surfaces...\n\n')
             end
-            if (verb > 1)
-                progressbar('Relaxing CMS...')
-            end
             
             % Main loop
             dJ = Inf;
@@ -114,9 +111,6 @@ classdef ConcentricMaclaurinSpheroids < handle
                 t_z_pass = tic;
                 fprintf('Updating zetas....')
             end
-            if (verb > 1)
-                progressbar('updating zetas...')
-            end
             
             % Loop over layers (outer) and colatitudes (inner)
             nlayers = size(obj.zetas, 1);
@@ -125,15 +119,14 @@ classdef ConcentricMaclaurinSpheroids < handle
                 for alfa=1:nangles
                     obj.zetas(ii,alfa) = obj.zeta_j_of_mu(ii, obj.mus(alfa));
                 end
-                if (verb > 1)
-                    progressbar(ii/nlayers);
-                end
             end
             
             % Optional communication
-            if (verb > 0)
+            if (verb > 1)
                 t_z_pass = toc(t_z_pass);
-                fprintf('Done. (%g sec.)\n', t_z_pass)
+                fprintf('done. (%g sec.)\n', t_z_pass)
+            elseif (verb > 0)
+                fprintf('done.\n')
             end
         end
         
@@ -158,9 +151,11 @@ classdef ConcentricMaclaurinSpheroids < handle
             end
             
             % Optional communication
-            if (verb > 0)
+            if (verb > 1)
                 t_J_pass = toc(t_J_pass);
-                fprintf('Done. (%g sec.)\n', t_J_pass)
+                fprintf(' (%g sec).\n', t_J_pass)
+            elseif (verb > 0)
+                fprintf('done.\n')
             end
         end
     end % public methods
@@ -471,6 +466,86 @@ switch n
 end
 end
 
+function [x,w] = gauleg(x1,x2,n)
+%GAULEG Calculate abscissas and weights for Gauss-Legendre n-point quadrature.
+%   [x,w] = GAULEG(x1,x2,n) returns the abscissas x and weights w that can be
+%   used to evaluate the definite integral, I, of a function well approximated
+%   by an (2n - 1) degree polynomial in the interval [x1,x2] using the
+%   Gauss-Legendre formula:
+%
+%       I = (x2 - x1)/2 * sum(w.*f(x))
+%
+%   Algorithm
+%     This function is based on the C++ implementation of a routine with the
+%     same name in Numerical Recipes, 3rd Edition. But in several places I opt
+%     for readability over performance, on the assumption that this function is
+%     most likely to be called in a setup routine rather than in an inner-loop
+%     computation.
+%
+%   Example
+%     fun = @(x)sin(x);
+%     [x,w] = gauleg(0,pi,6);
+%     I_adaptive = integral(fun,0,pi)
+%     I_gaussleg = (pi/2)*sum(w.*fun(x))
+%         
+% Author: Naor Movshovitz (nmovshov at google dot com)
+%         Earth and Planetary Sciences, UC Santa Cruz
+%
+% Reference: William H. Press, Saul A. Teukolsky, William T. Vetterling, and
+% Brian P. Flannery. 2007. Numerical Recipes 3rd Edition: The Art of Scientific
+% Computing (3 ed.). Cambridge University Press, New York, NY, USA.
+
+%% Input parsing and minimal assertions
+narginchk(3,3)
+nargoutchk(2,2)
+validateattributes(x1,{'numeric'},{'scalar','finite','real'},1)
+validateattributes(x2,{'numeric'},{'scalar','finite','real'},2)
+validateattributes(n,{'numeric'},{'scalar','finite','integer','>=',2},3)
+assert(x2 > x1, 'Interval must be positive.');
+
+%% Local variables
+tol = 1e-14;
+m = ceil(n/2);
+xmid = (x1 + x2)/2;
+dx = (x2 - x1);
+x = NaN(1,n);
+w = NaN(1,n);
+
+%% Main loop
+for j=1:m
+    % Get j-th root of Legenre polynomial Pn, along with Pn' value there.
+    z = cos(pi*((j - 1) + 0.75)/(n + 0.5)); % initial guess for j-th root
+    while true
+        % Calculate Pn(z) and Pn-1(z) and Pn'(z)
+        p = NaN(1,n+1);
+        p(1) = 1;
+        p(2) = z;
+        for k=2:n
+            pkm1 = p(k);
+            pkm2 = p(k-1);
+            pk = (1/k)*((2*k - 1)*z*pkm1 - (k - 1)*pkm2);
+            p(k+1) = pk;
+        end
+        pn = p(end);
+        pp = (n*p(end-1) - n*z*p(end))/(1 - z^2);
+        
+        % And now Newton's method (we are hopefully very near j-th root)
+        oldz = z;
+        z = z - pn/pp;
+        if abs(z - oldz) < tol, break, end
+    end
+    
+    % Now use j-th root to get 2 abscissas and weights
+    x(j)     = xmid - z*dx/2; % Scaled abscissa left of center
+    x(n+1-j) = xmid + z*dx/2; % Scaled abscissa right of center
+    w(j)     = 2/((1 - z^2)*pp^2);
+    w(n+1-j) = w(j);
+end
+
+%% Verify and return
+assert(all(isfinite(x)))
+assert(all(isfinite(w)))
+end
 
 %TODO: Gauss-Legendre abscissas do not include end points. Do some special
 %treatment to calculate zeta(equator) and zeta(north pole).
@@ -478,7 +553,3 @@ end
 %TODO: calls to Pn(k,mu) inside eq50() and eq51() are using cycles and are
 %unnecessary for gauss method. Figure out if where we can branch away from
 %adaptive integrals, or if we even want to keep adaptive option.
-
-%TODO: Figure out if you want gauleg as a class-related function, global
-%function, seed for a +utils package, or what?
-
