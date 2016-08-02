3c46e9a360758e69b6b6a90d01e83af39934549f
Naor Movshovitz <nmovshov@gmail.com>
2016-08-02 15:46:57 -0700

    constant_density_ellipsoid.m: implemented dubious ellipsoid m/l finding as in hubbard's test_GQ_Jn.pro maybe i'm missing something
    Former-commit-id: 49af346b50a3653dd608f89f9a71ad10cbf54be5

--- constant_density_ellipsoid.m
+++ constant_density_ellipsoid.m
@@ -19,14 +19,25 @@ cms = ConcentricMaclaurinSpheroids(opts);
 %% Converge cms (this may take a while)
 cms.relax
 
-%% Get top level surface
+%% Get surface layer shape
 a = cms.zeta_j_of_mu(1,0); % should be 1.0!
 b = cms.zeta_j_of_mu(1,1);
 mu = [0, cms.mus, 1];
 xi_cms = [a, cms.zetas(1,:), b];
+assert(abs(a - 1) < 1e-12)
 
-%% Construct ellipsoid
-el = sqrt((a/b)^2 - 1);
+%% Construct Maclaurin ellipsoid
+% Iterate to converge on Maclaurin ell parameter (?!?!)
+q = opts.qrot;
+m = 0.001;
+for k=1:20
+    lfun = @(x)(3./(2*x.^3)).*((3 + x.^2).*atan(x) - 3*x) - m;
+    el = fzero(lfun,0.5);
+    b_Mac = sqrt(1/(1 + el^2));
+    s_Mac = b_Mac; % s=b*a^2, a=1
+    m = q*s_Mac^3; % m=q*s^3/a^3, a=1
+end
+fprintf('m=%g; l=%g\n',m,el)
 xi_ell = 1./sqrt((1 + (el.^2).*(mu.^2)));
 
 %% Compare
