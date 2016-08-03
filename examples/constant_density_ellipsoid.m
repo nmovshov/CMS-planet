3f6a9dc6be31764b1148e25b5b8f1b610d40fa1e
Naor Movshovitz <nmovshov@gmail.com>
2016-08-03 15:55:09 -0700

    Former-commit-id: c29385b2dfada3b66d545287bdfca806e10dfc48

--- examples/constant_density_ellipsoid.m
+++ examples/constant_density_ellipsoid.m
@@ -33,38 +33,58 @@ addpath(fullfile(pwd,'..'))
 m = 0.1; % small rotation parameter
 lfun = @(x)(3./(2*x.^3)).*((3 + x.^2).*atan(x) - 3*x) - m;
 el = fzero(lfun,0.5); % ellipse parameter
-b = sqrt(1/(1 + el^2)); % polar radius
-s3 = b; % *mean* radius, s^3=b*a^2 (but a=1) we will use this later
+b_exact = sqrt(1/(1 + el^2)); % polar radius
+s3 = b_exact; % *mean* radius, s^3=b*a^2 (but a=1) we will use this later
 xi_exact = @(mu)1./sqrt((1 + (el^2).*(mu.^2)));
 
-% Let's take a quick look
-theta = linspace(0,2*pi);
-polax = polarplot(theta, xi_exact(cos(theta)));
-polax.Parent.ThetaZeroLocation = 'top';
-polax.Parent.ThetaDir = 'clockwise';
-polax.Parent.ThetaAxisUnits = 'rad';
+% Take a quick look for sanity check
+try % requires R2016a or later
+    theta = linspace(0,2*pi);
+    polax = polarplot(theta, xi_exact(cos(theta)));
+    polax.Parent.ThetaZeroLocation = 'top';
+    polax.Parent.ThetaDir = 'clockwise';
+    polax.Parent.ThetaAxisUnits = 'rad';
+catch
+end
 
-
-%% Set up a CMS object to mimic constant density case
-opts = cmsset;
-opts.nlayers = 1;
-opts.rcore = 0.01;
-opts.qrot = 0.1;
-opts.kmax = 32;
-opts.dJtol = 1e-14;
-opts.verbosity = 2;
+%% Now set up a CMS object to mimic constant density case
+q = m/s3; % CMS method uses q=w^2a^3/GM as rotation parameter
+opts = cmsset('qrot', q,...
+              'nlayers', 1,... % can be any number though!
+              'kmax', 32,...
+              'verbosity', 2);
 cms = ConcentricMaclaurinSpheroids(opts);
+cms.deltas = [1, 0*(2:opts.nlayers)];
 
-%% Converge cms (this may take a while)
+%% Converge cms (this may take a minute if using many layers)
 cms.relax
 
 %% Get surface layer shape
-a = cms.zeta_j_of_mu(1,0); % should be 1.0!
-b = cms.zeta_j_of_mu(1,1);
+a_cms = cms.zeta_j_of_mu(1,0); % should be 1.0!
+assert(abs(a_cms - 1) < 1e-12)
+b_cms = cms.zeta_j_of_mu(1,1);
 mu = [0, cms.mus, 1];
-xi_cms = [a, cms.zetas(1,:), b];
-assert(abs(a - 1) < 1e-12)
+xi_cms = [a_cms, cms.zetas(1,:), b_cms];
 
-%% Compare
-dxi = xi_cms - xi_exact;
-semilogy(mu, abs(dxi))
+% Take a quick look for sanity check
+try % requires R2016a or later
+    polax = polarplot(acos(mu), xi_cms);
+    polax.Parent.ThetaZeroLocation = 'top';
+    polax.Parent.ThetaDir = 'clockwise';
+    polax.Parent.ThetaAxisUnits = 'rad';
+catch
+end
+
+%% Compare numerical and analytic solutions
+dxi = xi_cms - xi_exact(mu);
+lh = semilogy(mu, abs(dxi));
+ah = lh.Parent;
+ah.XLabel.Interpreter = 'latex';
+ah.YLabel.Interpreter = 'latex';
+ah.Title.Interpreter = 'latex';
+ah.XLabel.String = '$\mu = \cos(\theta)$';
+ah.YLabel.String = '$d\xi$';
+ah.Title.String = '$d\xi = \xi(\mu) - 1/\sqrt{1 + l^2\mu^2}$';
+
+xi_err = max(abs(dxi));
+b_err = b_cms - b_exact;
