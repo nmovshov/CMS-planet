3ab8d372a158b7b7cd817267aecb6c266a5cc0b9
Naor Movshovitz <nmovshov@gmail.com>
2016-08-26 16:09:46 -0700

    Former-commit-id: 3a4349e612fb8f33623c18491228b93e3b25fb76

---
+++ examples/convergence_tests.m
@@ -0,0 +1,11 @@
+%% CMS CONVERGENCE TESTS
+% This script runs some numerical tests designed to verify convergence and help
+% us choose good parameters.
+
+%% Prepare workspace
+clear
+clc
+close all
+addpath(fullfile(pwd,'..'))
+
+%% Test convergence of Laplace expansion
