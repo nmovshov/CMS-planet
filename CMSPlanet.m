2f0f5acb38238f757a56bf4b0fa94e3368b2e322
Naor Movshovitz <nmovshov@gmail.com>
2016-09-12 21:24:23 -0700

    interim commit: I changed my mind again. I can't justify basing CMSPlanet class on CMS class, it does not stand in a isa relation. I go back to holding a CMS object as a public property of CMSPlanet. Makes everything a bit convoluted, and probably brittle, but who will know?

--- CMSPlanet.m
+++ CMSPlanet.m
@@ -1,4 +1,4 @@
-classdef CMSPlanet < ConcentricMaclaurinSpheroids
+classdef CMSPlanet < handle
     %CMSPLANET Interior model of rotating fluid planet.
     %   This class implements a model of a rotating fluid planet using the
     %   Concentric Maclaurin Spheroids technique. A CMSPlanet object is defined
@@ -8,6 +8,15 @@ classdef CMSPlanet < ConcentricMaclaurinSpheroids
     properties (Access = public)
         name
         desc
+        opts
+        cms
+        baro
+    end
+    properties (Dependent)
+        a0      % equatorial radius
+        M       % total mass
+        qrot    % rotation parameter 
+        nlayers % layers of constant density 
     end
     
     %% The constructor
@@ -15,30 +24,44 @@ classdef CMSPlanet < ConcentricMaclaurinSpheroids
         function obj = CMSPlanet(varargin)
             %CMSPLANET Class constructor.
             
-            % Call the base class constructor
-            obj@ConcentricMaclaurinSpheroids(varargin{:});
-
+            % Empty, integer, or name/value arguments.
+            warning off CMS:obsolete
+            if nargin == 0
+                op = cmsset();
+            else
+                if isnumeric(varargin{1})
+                    varargin = {'nlayers',varargin{1},varargin{2:end}};
+                end
+                op = cmsset(varargin{:});
+            end
+            warning on CMS:obsolete
+            
+            % Save opts struct and call InitPlanet to finish the construction
+            obj.opts = op; % (calls set.opts and may have side effects!)
+            obj.InitPlanet(op);
         end
-    end % constructor block
+    end % End of constructor block
     
     %% Public methods
     methods (Access = public)
         
-    end % public methods
+    end % End of public methods block
     
     %% Private methods
     methods (Access = private)
-        
-    end % private methods
+        function InitPlanet(obj,op)
+           % (Re)Initialize a CMSPlanet object. 
+        end
+    end % End of private methods block
     
     %% Access methods
     methods
         
-    end % access methods
+    end % End of access methods block
     
     %% Static methods
     methods (Static)
         
-    end % static methods
+    end % End of static methods block
 end % End of classdef
 
