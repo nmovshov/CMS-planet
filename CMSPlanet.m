de29820d25840049a57f5dbcf37da6d7c57aa9c0
Naor Movshovitz <nmovshov@gmail.com>
2016-09-13 17:05:35 -0700

    reordered props renamed baro -> eos implemented setters for M and a0 added private prop si for no reason
    Former-commit-id: 47ed501bfa39303b9c443f7d8db1d8427ff9aa38

--- CMSPlanet.m
+++ CMSPlanet.m
@@ -10,14 +10,17 @@ classdef CMSPlanet < handle
         desc    % model description (one line)
         a0      % equatorial radius
         M       % total mass
-        opts    % struct with opts, both CMS and CMSPlanet
         cms     % a CMS object
-        baro    % a barotrope object
+        eos     % a barotrope object
+        opts    % struct with opts, both CMS and CMSPlanet
     end
     properties (Dependent)
         qrot    % rotation parameter 
         nlayers % layers of constant density 
         Jn      % external gravity moments
+    end
+    properties (Access = private)
+        si
     end
     
     %% The constructor
@@ -82,6 +85,28 @@ classdef CMSPlanet < handle
         
         function val = get.Jn(obj)
             val = obj.cms.Jn;
+        end
+        
+        function set.M(obj,val)
+            try
+                assert(isnumeric(val))
+                assert(isscalar(val))
+                assert(double(val) > 0)
+                obj.M = val;
+            catch
+                error('Mass must be a positive scalar.')
+            end
+        end
+        
+        function set.a0(obj,val)
+            try
+                assert(isnumeric(val))
+                assert(isscalar(val))
+                assert(double(val) > 0)
+                obj.a0 = val;
+            catch
+                error('Radius must be a positive scalar.')
+            end
         end
     end % End of access methods block
     
