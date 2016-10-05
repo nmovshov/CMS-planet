74c69e95d4e486a32df9f52662d1535b660b5865
Naor Movshovitz <nmovshov@gmail.com>
2016-10-05 15:30:36 -0700

    interim commit working on index_1_polytrope.m modified Polytrope.m constructor to throw usage msg before error and also made all properties read-only.
    Former-commit-id: fa9f4c0f6b799711a7b9cc35c6b8780739735e9b

--- +barotropes/Polytrope.m
+++ +barotropes/Polytrope.m
@@ -2,7 +2,7 @@ classdef Polytrope < barotropes.Barotrope
     %POLYTROPE A barotrope of the form P = K*rho^(1 + 1/n).
     
     %% Properties
-    properties (Access = public)
+    properties (SetAccess = private)
         K % polytropic constant, dimensions depend on index
         n % polytropic index, dimensionless
         alpha % 1 + 1/n
@@ -11,13 +11,21 @@ classdef Polytrope < barotropes.Barotrope
     %% The constructor
     methods
         function obj = Polytrope(K, n)
-            if nargin > 0
+            if (nargin == 0) && (nargout == 0)
+                print_usage()
+                return
+            end
+            try
+                narginchk(2,2)
                 assert(isnumeric(K) && isscalar(K) && double(K) > 0)
                 assert(isnumeric(n) && isscalar(n) && double(n) > 0)
-                obj.K = K;
-                obj.n = n;
-                obj.alpha = 1 + 1/n;
+            catch ME
+                print_usage()
+                rethrow(ME)
             end
+            obj.K = K;
+            obj.n = n;
+            obj.alpha = 1 + 1/n;
         end
     end
     
@@ -35,6 +43,12 @@ classdef Polytrope < barotropes.Barotrope
             rho = (P/obj.K).^(1/obj.alpha);
         end
     end
-    
 end
 
+%% Usage message
+function print_usage()
+fprintf('Usage: barotropes.Polytrope(K, n)\n')
+fprintf('positional arguments:\n')
+fprintf('  K    polytropic constant (positive real scalar)\n')
+fprintf('  n    polytropic index (positive real scalar)\n')
+end
