c67c9ee1593f135e7b88cb38ac01c70db878d2fd
Naor Movshovitz <nmovshov@gmail.com>
2016-10-24 11:25:49 -0700

    Develop (#5)
    * test of develop branch pushing
    
    * used profiler to remove an unexpected double call to cms.get.Pi() when indexed directly.
    
    * tweaking command window communication some more
    
    * renaming opts.MaxIter->opts.MaxIterHE to distinguish from opts.MaxIterBar
    
    * interim commit:
    adding a P_mid property to CMSPlanet.m to obtain mid-layer pressures
    working on nonrotating_polytrope.m
    
    * functioning CMSPlanet.m::relax_to_barotrope. I use a single convergnece measure dBar against a single tolerance param opts.dBtol. The check is the maximum of mass matching error (1-dM), average density change, and variance of density change.
    renamed dMtol to dBtol in cmsset.m
    removed index_1_polytrope.m in preparation of merging back to master
    still working on nonrotating_polytrope.m
    
    * in relax_to_barotrope check against dBtol should use abs(dBar)
    suppressing mlint msg about si vs obj.si
    
    * CMSPlanet.m::relax_to_barotrope: changed my mind about the convergence criteria. I think we will use only the variance of rho changes as it is the one measure that WILL decrease monotonically to zero. But I may change my mind again after talking to Bill
    
    * more work on nonrotating_polytrope.m
    almost ready to say it's working. I am curious about the remaining error at the innermost and outermost layers. Even at 128 layers, when the rest of the profile is right on the analytic curve. Is this anything?
    
    * finishing up plotting of nonrotating_polytrope.m
    
    * I _think_ the nonrotating_polytrope.m example is working. It looks like we need more than 128 layers to get a reasonable agreement with the analytic solution and there is always a noticeable pressure deficit at the center. Something to look into.

---
+++ examples/nonrotating_polytrope.m
@@ -0,0 +1,102 @@
+%% INTERIOR STRUCTURE OF A NON-ROTATING INDEX 1 POLYTROPE
+% Example and test of the CMSPlanet class. We construct and converge a
+% model of a non-rotating fluid planet with the pressure-density law
+%
+% $$P = K\rho^2$$
+%
+% with a polytropic constant $K$. This script demonstrates how to set up a
+% CMSPlanet object with a specified barotrope and converge to a density
+% structure in hydrostatic equilibrium with the given barotrope. The default
+% starting density profile is that of a homogeneous sphere.
+%
+% Reminder: the density as a function of radius of a non-rotating, index-1
+% polytropic planet in hydrostatic equilibrium is
+% 
+% $$\rho(r) = \rho_c \sin(ar)/(ar)$$
+%
+% where
+% 
+% $$a = \sqrt{2\pi{G}/K}$$
+% 
+% and
+% 
+% $$\rho_c = 3M/(4\pi{R^3})$$
+%
+% where $M$ is the planet's mass and $R = \pi/a$, which is independent of mass.
+% 
+
+%% Prepare workspace
+clear
+clc
+close all
+addpath(fullfile(pwd,'..'))
+try
+    si = setUnits; % if you have physunits
+catch
+    si = setFUnits; % if you don't have physunits
+end
+G = si.gravity;
+
+%% Set up a CMS Planet with arbitrary mass and radius
+M = 317.8*si.earth_mass;
+R = 71492*si.km;
+
+cmp = CMSPlanet(16);
+cmp.name = 'nrpoly';
+cmp.desc = 'A nonrotating polytropic planet';
+cmp.M = M;
+cmp.a0 = R;
+cmp.qrot = 0;
+
+%% Construct a polytrope of index 1 to represent the planet's eos
+% A non-rotating, index-1 polytrope is completely defined by K. The radius is
+% independent of mass. The density structure normalized to the central density
+% is also independent of mass, and the absolute valaue of density is determined
+% with the known average density.
+n = 1; % polytrope index
+K = 2*G/pi*R^2; % polytrope constant
+eos = barotropes.Polytrope(K, n);
+cmp.eos = eos;
+
+%% Relax to desired barotrope
+cmp.opts.verbosity = 1;
+cmp.opts.dBtol = 0.0001;
+cmp.opts.MaxIterBar = 10;
+cmp.relax_to_barotrope;
+
+%% Compare computed and analytic density structure
+% prepare
+set(groot, 'defaultTextInterpreter', 'latex')
+set(groot, 'defaultLegendInterpreter', 'latex')
+set(groot, 'defaultAxesBox', 'on')
+
+% calculate
+a = sqrt(2*pi*G/K);
+R = pi/a;
+rho_av = 3*cmp.M_calc/(4*pi*R^3);
+rho_c = (pi^2/3)*rho_av;
+r = linspace(0,1)*cmp.a0;
+rho_exact = rho_c*sin(a*r)./(a*r);
+rho_exact(1) = rho_c;
+
+% plot
+ah = axes; hold(ah);
+l1 = plot(r/cmp.a0, rho_exact/rho_c);
+l2 = stairs(cmp.ai/cmp.a0, cmp.rhoi/rho_c, '-');
+
+% annotate
+l1.DisplayName = '$\sin(ar)/(ar)$';
+l2.DisplayName = '$\rho_i/\rho_c, i=1,\ldots{},N$';
+
+xlabel('$r/a_0$')
+ylabel('$\rho/\rho_c$')
+
+legend(ah, 'show')
+
+% errors
+P_err = (cmp.P_c - cmp.eos.pressure(rho_c))/cmp.eos.pressure(rho_c);
+M_err = (cmp.M_calc - cmp.M)/cmp.M;
+s_tit = sprintf(['$N_\\mathrm{layers}=%g$; ',...
+    '$\\Delta M_\\mathrm{tot}=%g\\%%$; $\\Delta P_c=%g\\%%$'],...
+    cmp.nlayers, double(M_err)*100, double(P_err)*100);
+title(s_tit)
