4d912e6a12c9b396cd654a470b00242321acd717
Naor Movshovitz <nmovshov@gmail.com>
2016-10-26 15:54:11 -0700

    I think index_1_polytrope.m was working all along but needed many more layers to start delivering good numbers. The Js are independent of K, as they should be! going to run with N=512 before merging back to master.

--- examples/index_1_polytrope.m
+++ examples/index_1_polytrope.m
@@ -25,40 +25,57 @@ end
 G = si.gravity;
 
 %% Set up a CMS Planet with arbitrary mass and radius
+% I'm using numbers for Jupiter just for kicks, no effect on Js of course.
 M = 317.8*si.earth_mass;
 R = 71492*si.km;
 
-cmp = CMSPlanet(16);
+cmp = CMSPlanet(512);
 cmp.name = 'poly1';
 cmp.desc = 'An index-1 polytropic planet';
 cmp.M = M;
 cmp.a0 = R;
-cmp.qrot = 0.089195487; % used in Hubbard (2013)
+cmp.qrot = 0.089195487; % Hubbard (2013) Table 5
 
 %% Construct a polytrope of index 1 to represent the planet's eos
-% The initial value of K is chosen to match the given radius if the planet were
-% not rotating. We cannot expect an exact match when we specify mass, radius,
-% and eos at the same time as this over determines the planet's structure. The
-% CMS method will try to match the density structure to the barotrope and
-% equatorial radius, leaving the total mass different from the specified value.
-% Then the polytropic constant is adjusted to match the total mass. I _think_
-% this is what is done in Hubbard (2013).
-n = 1; % polytrope index
-K = 2*G/pi*R^2; % polytrope constant
+n = 1;
+K = 2*G/pi*R^2; % ...matches radius just for show, K has no effect on the Js
 eos = barotropes.Polytrope(K, n);
 cmp.eos = eos;
 
 %% Relax to desired barotrope
-cmp.opts.verbosity = 1;
+cmp.opts.verbosity = 3;
 cmp.opts.dBtol = 0.0001;
 cmp.opts.MaxIterBar = 10;
+cmp.opts.email = 'nmovshov@gmail.com';
 cmp.relax_to_barotrope;
 
 %% Compare computed and analytic density structure
 q = cmp.qrot;
-ZT3 = [q; 1.3994099; 5.3871087; 3.9972442; nan; nan; nan];
+% Zharkov & Trubistyn (1978) eq. 34.12
+ZT3 = [q;...
+    (0.173273*q - 0.197027*q^2 + 0.15*q^3)*1e2;...
+    (-0.081092*q^2 + 0.15*q^3)*-1e4;...
+    (0.056329*q^3)*1e5;...
+    nan; nan; nan];
+% Hubbard (2013) Table 5
 H13_256 = [q; 1.3991574; 5.3182810; 3.0118323; 2.1321157; 1.7406710; 1.5682179];
 H13_512 = [q; 1.3989253; 5.3187997; 3.0122356; 2.1324628; 1.7409925; 1.5685327];
-CMP = [q; cmp.Js(2)*1e2; -cmp.Js(3)*1e4; cmp.Js(4)*1e5; -cmp.Js(5)*1e6; cmp.Js(6)*1e7; -cmp.Js(7)*1e8;];
+
+% CMSPlanet
+CMP = [q; cmp.Js(2)*1e2; -cmp.Js(3)*1e4; cmp.Js(4)*1e5; -cmp.Js(5)*1e6;...
+    cmp.Js(6)*1e7; -cmp.Js(7)*1e8;];
+
+% Make it a table
 T = table(ZT3, H13_256, H13_512, CMP);
-T.Properties.RowNames = {'q','J2x10^2','-J4x10^4','J6x10^5','-J8x10^6','J10x10^7','-J12x10^8'};
+T.Properties.RowNames = {'q','J2x10^2','-J4x10^4','J6x10^5','-J8x10^6',...
+    'J10x10^7','-J12x10^8'};
+
+% Display
+format long
+format compact
+disp(T)
+format
+
+%% Save and deliver
+save('index1polytrope', 'cmp', 'T')
+sendmail('nmovshov@gmail.com','n=1 polytrope','','index1polytrope.mat')
