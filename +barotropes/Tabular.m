ec36c5ecae8d52235d2c9f42f05c8da381709565
Naor Movshovitz <nmovshov@gmail.com>
2016-12-04 23:15:47 -0800

    implemented interp1 features in Tabular.m. Behavior should be correct but note that the error messaged thrown by interp1 can be misleading. I'll maybe fix it some day by doing more detailed input parsing in the base constructor. For now user beware, or just use defaults!
    Former-commit-id: 25cf2e532684624ac31b158c6f606a636ae26bcb

--- +barotropes/Tabular.m
+++ +barotropes/Tabular.m
@@ -1,43 +1,40 @@
-classdef tabular < barotropes.Barotrope
+classdef Tabular < barotropes.Barotrope
     %TABULAR A base class for a generic table barotrope.
     
     %% Properties
     properties (SetAccess = protected)
-        mP   % a vector of pressure values
-        mrho % a vector of density values
+        P_vals   % a vector of pressure values
+        rho_vals % a vector of density values
     end
     properties (Access = public)
-        interp_method
-        extrap_method
+        interpolation_method
+        extrapolation_method
         meta
     end
     
     %% The constructor
     methods
-        function obj = tabular(P, rho, intm, extm)
+        function obj = Tabular(P, rho, intm, extm)
             if (nargin == 0) && (nargout == 0)
                 print_usage()
                 return
             end
             try
                 narginchk(2,4)
+                if nargin < 4, extm = NaN; end
+                if nargin < 3, intm = 'linear'; end
                 assert(isnumeric(P) && isvector(P) && all(double(P) >= 0))
                 assert(isnumeric(rho) && isvector(rho) && all(double(rho) >= 0))
-                assert(length(P) == length(rho))                
+                assert(length(P) == length(rho))
+                assert(interp1(P, rho, P(1), intm, extm) == rho(1))
             catch ME
                 print_usage()
                 rethrow(ME)
             end
-            obj.mP = P;
-            obj.mrho = rho;
-%             if nargin > 2, obj.interp_method = intm; end
-%             if nargin > 3, obj.extrap_method = extm; end
-%             try
-%                 interp1(P, rho, P(1), obj.interp_method, obj.extrap_method);
-%             catch ME
-%                 print_usage()
-%                 rethrow(ME)
-%             end
+            obj.P_vals = P;
+            obj.rho_vals = rho;
+            obj.interpolation_method = intm;
+            obj.extrapolation_method = extm;
         end
     end
     
@@ -58,7 +55,7 @@ classdef tabular < barotropes.Barotrope
 end
 
 %% Usage message
-function print_usage()
+function print_usage() %TODO: rewrite
 fprintf('Usage: barotropes.tabular(P, rho)\n')
 fprintf('positional arguments:\n')
 fprintf('  P    a vector of pressure values (real nonnegative)\n')
