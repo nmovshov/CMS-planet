3c269fed91def28cc1ccf1a5c247e4ecc71284b1
Naor Movshovitz <nmovshov@gmail.com>
2017-01-23 13:50:51 -0800

    redefining core to be the last layer with normalized radius adjust rather than a bunch of const density layers extending to the desired radius, which was a waste of layers; also adding .gitignore

--- +models/single_polytrope_w_core.m
+++ +models/single_polytrope_w_core.m
@@ -2,15 +2,15 @@ function cmp = single_polytrope_w_core(N, x)
 %SINGLE_POLYTROPE_W_CORE Toy model planet approximated by a polytrope and core.
 %    SINGLE_POLYTROPE_CORE(N, x) returns an N-layer CMSPlanet object constructed
 %    with a barotropes.Polytrope eos using the polytropic constant x(1) and the
-%    polytropic index x(2) applied to lower-indexed layers and a
-%    barotropes.ConstDensity eos with density x(3) applied to higher-indexed
-%    layers. Transition is at layer index fix(N*(1 - x(4))). Layer equatorial
-%    radii are equally spaced.
+%    polytropic index x(2) applied to the first N-1 layers and a
+%    barotropes.ConstDensity eos with density x(3) applied to last layer. Layer
+%    equatorial radii are equally spaced with the normalized radius of the last
+%    layer (the "core") set to x(4).
 
 cmp = CMSPlanet(N);
 eos1 = barotropes.Polytrope(x(1), x(2));
 eos2 = barotropes.ConstDensity(x(3));
-tind = fix(N*(1 - x(4)));
-cmp.eos = [repmat(eos1, tind, 1); repmat(eos2, N - tind, 1)];
+cmp.cms.lambdas = linspace(1, x(4), N);
+cmp.eos = [repmat(eos1, N-1, 1); eos2];
 
 end
