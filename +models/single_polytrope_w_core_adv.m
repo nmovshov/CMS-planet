e9797741052b1979fff9d911f4505ab4014ea333
Naor Movshovitz <nmovshov@gmail.com>
2017-01-25 22:12:07 -0800

    interim commit: advanced single poly with core implemented BUT NOT TESTED; linting other files

--- +models/single_polytrope_w_core_adv.m
+++ +models/single_polytrope_w_core_adv.m
@@ -1,18 +1,32 @@
 function cmp = single_polytrope_w_core_adv(N, x)
-%SINGLE_POLYTROPE_W_CORE_ADV Model Polytrope-and-core model planet (advanced).
-%    SINGLE_POLYTROPE_W_CORE_ADV(N, x) returns an N-layer CMSPlanet object 
-%    with a barotropes.Polytrope with constant x(1) and polytropic index x(2)
-%    assigned to layers 2:N-1 and a barotropes.ConstDensity eos with rho0 = x(3)
-%    assigned to layer N, which has a normalized equatorial radius equal to x(4).
-%    Layer 1 is assigned a zero density barotropes.ConstDensity eos. The layer
-%    spacing is designed to minimize discretization error by concentrating 2/3 of
-%    the available layers in the top 0.5 of the planet (see Hubbard & Militzer, 
-%    2016).
+%SINGLE_POLYTROPE_W_CORE_ADV Polytrope-and-core planet (advanced variation).
+%    SINGLE_POLYTROPE_W_CORE_ADV(N, x) returns an N-layer CMSPlanet object with
+%    a barotropes.Polytrope with constant x(1) and index x(2) assigned to layers
+%    2:N-1 and a barotropes.ConstDensity eos with rho0=x(3) assigned to layer N
+%    which has a normalized equatorial radius equal to x(4). Layer 1 is assigned
+%    a zero density barotropes.ConstDensity eos. The layer spacing is designed
+%    to minimize discretization error by concentrating 2/3 of the available
+%    layers in the top 0.5 of the planet (see Hubbard & Militzer, 2016). If
+%    x(4)>=0.2 then layers are equally spaced above x(4).
 
 cmp = CMSPlanet(N);
+
+if x(4) >= 0.2
+    dl = (1 - x(4))/N;
+    cmp.cms.lambdas = [1, linspace(1 - dl/2, x(4), N - 1)]';
+else
+    n1 = fix(2/3*N) - 1;
+    n2 = N - n1 - 1;
+    dl1 = 0.5/n1;
+    dl2 = (0.5 - x(4))/n2;
+    lam1 = linspace(1 - dl1/2, 0.5, n1);
+    lam2 = linspace(0.5 - dl2, x(4), n2);
+    cmp.cms.lambdas = [1, lam1, lam2]';
+end
+
+eos0 = barotropes.ConstDensity(0);
 eos1 = barotropes.Polytrope(x(1), x(2));
 eos2 = barotropes.ConstDensity(x(3));
-cmp.cms.lambdas = linspace(1, x(4), N);
-cmp.eos = [repmat(eos1, N-1, 1); eos2];
+cmp.eos = [eos0; repmat(eos1, N - 2, 1); eos2];
 
 end
