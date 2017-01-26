eb110874c46dd7ef57e95d70590cfd1ddbce0701
Naor Movshovitz <nmovshov@gmail.com>
2017-01-25 22:12:07 -0800

    interim commit: advanced single poly with core implemented BUT NOT TESTED; linting other files
    Former-commit-id: e9797741052b1979fff9d911f4505ab4014ea333

--- +models/single_polytrope_adv.m
+++ +models/single_polytrope_adv.m
@@ -2,18 +2,17 @@ function cmp = single_polytrope_adv(N, x)
 %SINGLE_POLYTROPE_ADV The simplest toy model planet (advanced version).
 %    SINGLE_POLYTROPE_ADV(N, x) returns an N-layer CMSPlanet object with a
 %    barotropes.Polytrope with constant x(1) and index x(2) assigned to layers
-%    2:N. Layer 1 is assigned a zero density barotropes.ConstDensity eos. The 
+%    2:N. Layer 1 is assigned a zero density barotropes.ConstDensity eos. The
 %    layer spacing is designed to minimize discretization error by concentrating
 %    2/3 of the available layers in the top 0.5 of the planet (see Hubbard &
 %    Militzer, 2016).
 
 cmp = CMSPlanet(N);
 
-n1 = fix(2*N/3) - 1;
+n1 = fix(2/3*N) - 1;
 n2 = N - n1 - 1;
 dl1 = 0.5/n1;
 dl2 = 0.5/n2;
-
 lam1 = linspace(1 - dl1/2, 0.5, n1);
 lam2 = linspace(0.5 - dl2, dl2, n2);
 cmp.cms.lambdas = [1, lam1, lam2]';
