a456b030665f20b76e26664dceee24eafb200cf1
Naor Movshovitz <nmovshov@gmail.com>
2017-01-26 11:54:10 -0800

    fixed and tested sngl_poly reg and w core advanced variants to more closely track the radii distribution recommended by hubbard (not that it that important)
    Former-commit-id: 050e60d8702854faaa13edad44df91d8448585ff

--- +models/single_polytrope_w_core_adv.m
+++ +models/single_polytrope_w_core_adv.m
@@ -4,20 +4,22 @@ function cmp = single_polytrope_w_core_adv(N, x)
 %    a barotropes.Polytrope with constant x(1) and index x(2) assigned to layers
 %    2:N-1 and a barotropes.ConstDensity eos with rho0=x(3) assigned to layer N
 %    which has a normalized equatorial radius equal to x(4). Layer 1 is assigned
-%    a zero density barotropes.ConstDensity eos. The layer spacing is designed
-%    to minimize discretization error by concentrating 2/3 of the available
-%    layers in the top 0.5 of the planet (see Hubbard & Militzer, 2016). If
-%    x(4)>=0.2 then layers are equally spaced above x(4).
+%    a zero density barotropes.ConstDensity eos and is approximately half the
+%    width of the layers below. The layer spacing is designed to minimize
+%    discretization error by concentrating 2/3 of the available layers in the
+%    top 0.5 of the planet (see Hubbard & Militzer, 2016). However if x(4)>0.2
+%    then layers are equally spaced above x(4).
 
 cmp = CMSPlanet(N);
 
-if x(4) >= 0.2
+if x(4) > 0.2
     dl = (1 - x(4))/N;
     cmp.cms.lambdas = [1, linspace(1 - dl/2, x(4), N - 1)]';
 else
+    if x(4) <= 0, x(4) = eps; end
     n1 = fix(2/3*N) - 1;
     n2 = N - n1 - 1;
-    dl1 = 0.5/n1;
+    dl1 = 0.5/(n1 - 1);
     dl2 = (0.5 - x(4))/n2;
     lam1 = linspace(1 - dl1/2, 0.5, n1);
     lam2 = linspace(0.5 - dl2, x(4), n2);
