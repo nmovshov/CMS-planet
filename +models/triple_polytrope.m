184c6a348cd8259be5cf5476ff0b69e2f7e14d41
Naor Movshovitz <nmovshov@gmail.com>
2017-02-23 01:08:17 -0800

    added triple polytrope template model
    Former-commit-id: f3415ba9214481fe6ab7088f97bba70fa35abc71

---
+++ +models/triple_polytrope.m
@@ -0,0 +1,41 @@
+function cmp = triple_polytrope(N, x)
+%TRIPLE_POLYTROPE Model planet approximated by three polytropes.
+%    TRIPLE_POLYTROPE(N, x) returns an N-layer CMSPlanet object with three
+%    barotropes.Polytrope eos objects. First polytrope defined by constant x(1)
+%    and index x(2) is applied to layers 2:tind. Second polytrope defined by
+%    constant x(3) and index x(4) is applied to layers tind+1:cind. Third
+%    polytrope defined by constant x(5) and index x(6) is applied to layers
+%    cind+1:N. Transition from first to second polytrope is at layer
+%    tind=fix(N*(1-x(6))). Transition from second to third polytrope is at layer
+%    cind=fix(N*(1-x(7))). Layer 1 is assigned a zero density
+%    barotropes.ConstDensity eos and is approximately half the width of the layers
+%    below. The layer spacing is designed to minimize discretization error by
+%    concentrating 2/3 of the available layers in the top 0.5 of the planet (see
+%    Hubbard & Militzer, 2016).
+
+cmp = CMSPlanet(N);
+
+n1 = fix(2/3*N) - 1;
+n2 = N - n1 - 1;
+dl1 = 0.5/(n1 - 1);
+dl2 = 0.5/(n2 + 1);
+lam1 = linspace(1 - dl1/2, 0.5, n1);
+lam2 = linspace(0.5 - dl2, dl2, n2);
+cmp.cms.lambdas = [1, lam1, lam2]';
+
+eos0 = barotropes.ConstDensity(0);
+eos1 = barotropes.Polytrope(x(1), x(2));
+eos2 = barotropes.Polytrope(x(3), x(4));
+eos3 = barotropes.Polytrope(x(5), x(6));
+tind = fix(N*(1 - x(7)));
+tind = min(max(tind, 1), N);
+cind = fix(N*(1 - x(8)));
+cind = min(max(cind, 1), N);
+assert(cind > tind,...
+    'Degenerate model: transition layers must be monotonic increasing.')
+cmp.eos = [eos0;...
+    repmat(eos1, tind - 1, 1);...
+    repmat(eos2, cind - tind, 1);...
+    repmat(eos3, N - cind, 1)];
+
+end
