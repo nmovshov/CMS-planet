1fe9ec743b366e91b40ef48cd3af97dd36b2b9b5
Naor Movshovitz <nmovshov@gmail.com>
2017-03-16 18:34:36 -0700

    lamstrat in double poly with core
    Former-commit-id: 919c13c29e11d3729e3242d5ebfd50223a0f91ce

--- +models/double_polytrope_w_core.m
+++ +models/double_polytrope_w_core.m
@@ -1,4 +1,4 @@
-function cmp = double_polytrope_w_core(N, x)
+function cmp = double_polytrope_w_core(N, x, lamstrat)
 %DOUBLE_POLYTROPE_W_CORE Two polytrope and core model planet.
 %    DOUBLE_POLYTROPE_W_CORE(N, x) returns an N-layer CMSPlanet object with two
 %    barotropes.Polytrope eos objects. First polytrope defined by constant x(1)
@@ -12,20 +12,35 @@ function cmp = double_polytrope_w_core(N, x)
 %    concentrating 2/3 of the available layers in the top 0.5 of the planet (see
 %    Hubbard & Militzer, 2016). However if x(7)>0.2 then layers are equally spaced
 %    above x(7).
+%
+%    DOUBLE_POLYTROPE_W_CORE(N, x, lamstrat) uses the 2-element vector lamstrat to
+%    specify the layer spacing strategy. Approximately lamstrat(1) of available
+%    layers will be distributed in the top lamstrat(2) of the planet. For example,
+%    passing [3/4, 0.2] concentrates the layers heavily in the top 20% of the
+%    planet, leaving about N/4 layers to fill the bottom 80%. Passing [r, r] gives
+%    approximately equal spacing throughout the planet. (Approximately because a
+%    single half-width layer of zero density is always reserved for the surface.)
+%    However if x(7)>0.2 or if x(7)>(1-lamstrat(2)) then we revert to equally
+%    spaced layers again. Note that when x(7) is close to (1-lamstrat(2)) you
+%    might be better of using equal spacing!
+
+narginchk(2,3)
+if nargin == 2, lamstrat = [2/3, 1/2]; end
+validateattributes(lamstrat, {'numeric'}, {'vector', 'numel', 2, '>', 0, '<', 1})
+if x(7) <= 0, x(7) = eps; end
 
 cmp = CMSPlanet(N);
 
-if x(7) > 0.2
+if (x(7) > 0.2) || (x(7) >= (1 - lamstrat(2)))
     dl = (1 - x(7))/N;
     cmp.cms.lambdas = [1, linspace(1 - dl/2, x(7), N - 1)]';
 else
-    if x(7) <= 0, x(7) = eps; end
-    n1 = fix(2/3*N) - 1;
+    n1 = fix(lamstrat(1)*(N - 1));
     n2 = N - n1 - 1;
-    dl1 = 0.5/(n1 - 1);
-    dl2 = (0.5 - x(7))/n2;
-    lam1 = linspace(1 - dl1/2, 0.5, n1);
-    lam2 = linspace(0.5 - dl2, x(7), n2);
+    dl1 = lamstrat(2)/(n1 - 1);
+    dl2 = (1 - lamstrat(2) - x(7))/(n2);
+    lam1 = linspace(1 - dl1/2, (1 - lamstrat(2)), n1);
+    lam2 = linspace((1 - lamstrat(2)) - dl2, x(7), n2);
     cmp.cms.lambdas = [1, lam1, lam2]';
 end
 
