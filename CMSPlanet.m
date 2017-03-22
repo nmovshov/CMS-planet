nmovshov/CMS-planet

commit 60490ec58ab3e0072705396fff672792e6008334

lightly tested CMP.core_mass(); letting relax_to_barotrop skip the HE part if q=0 and zeta=0 already

CMSPlanet.m
@@ -113,12 +113,16 @@ classdef CMSPlanet < handle
                     fprintf('Baropass %d (of max %d)...\n',...
                         iter, obj.opts.MaxIterBar)
                 end
-                obj.cms.update_zetas;
-                if isequal(obj.opts.equipotential_squeeze, 'polar')
-                    obj.cms.update_polar_radii;
+                if (obj.qrot > 0) || any(obj.cms.zetas(:) < 1)
+                    obj.cms.update_zetas;
+                    if isequal(obj.opts.equipotential_squeeze, 'polar')
+                        obj.cms.update_polar_radii;
+                    end
+                    dJ = obj.cms.update_Js;
+                    if (verb > 1), fprintf('  '), end
+                else
+                    dJ = 0;
                 end
-                dJ = obj.cms.update_Js;
-                if (verb > 1), fprintf('  '), end
                 dro = obj.update_densities;
                 
                 % The convergence tolerance is the max of dJs and drhos

commit 5f1dbe2ed4b72324727b26c78dbb78c80bb7df97

Fixed bug introduced in commit 60490ec58ab3e0072705396fff672792e6008334
When I let cmp.relax_to_barotrope skip the cms.update_zetas part if
qrot==0 I forgot that this also skips setting the flag to recalculate
the potential, for the purpose of recalculating the pressure, for the
purpose of recalculate the density. So cmp.update_densities was not
computed correctly. The fix was to call cms.update_Js, even though we
know all Js are 0 in this case. The call resets the right flags and the
pressure/density relation can be computed correctly, and very quickly
for a non-rotating planet.

CMSPlanet.m
@@ -121,7 +121,7 @@ classdef CMSPlanet < handle
                     dJ = obj.cms.update_Js;
                     if (verb > 1), fprintf('  '), end
                 else
-                    dJ = 0;
+                    dJ = obj.cms.update_Js;
                 end
                 dro = obj.update_densities;
                 