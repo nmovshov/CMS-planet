306b80e67502004567101ac038e1dbfe34be7e24
Naor Movshovitz <nmovshov@gmail.com>
2017-03-28 15:51:01 -0700

    standardizing lambda functions

--- +lambdas/equal_dr.m
+++ +lambdas/equal_dr.m
@@ -1,9 +1,19 @@
-%% EQUISPACED LAMBA DISTRIBUTION
-% Return a lambda distribution which is equispaced in radius.
-function lambdas = equi_spaced(N)
+function lambdas = equal_dr(N, halftop)
+%EQUAL_DR Return a lambda distribution with equally spaced radii.
+%    lambdas = EQUAL_DR(N) returns an N-vector of equally spaced normalized radii.
+%
+%    lambdas = EQUAL_DR(N, halftop) where halftop==true modifies the 2nd element
+%    of lambdas to make the thickness of the first layer be exactly half that of
+%    the second layer. The default is halftop=true.
 
-lambdas = linspace(1/N, 1, N);
+narginchk(1,2)
 
-lambdas = flip(lambdas);
+lambdas = linspace(1, 1/N, N)';
+
+if nargin == 1, halftop = true; end
+validateattributes(halftop, {'logical'}, {'scalar'})
+if halftop
+    lambdas(2) = 1 - (1 - lambdas(3))/3;
+end
 
 end
