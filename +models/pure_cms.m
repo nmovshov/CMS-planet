7bc72be02f6bf310e2365f64aee1f23244a0d6ee
Naor Movshovitz <nmovshov@gmail.com>
2017-05-29 21:51:35 -0700

    Single input variant of models.pure_cms().
    To streamline passing this model to fminsearch and mhsample
    we allow both one- and two-argument calls:
      cmp = models.pure_cms([lams, dels])
      cmp = models.pure_cms(lams, dels)
    
    
    Former-commit-id: a851ac97d45b033abb3ff37fb6772667ac257030

--- +models/pure_cms.m
+++ +models/pure_cms.m
@@ -8,8 +8,21 @@ function cmp = pure_cms(lams, dels)
 %   Obviously this can be easily done in any script or in the Command Window but
 %   some driver scripts require a handle to a parametrized model-building
 %   function, so we supply one.
+%
+%   PURE_CMS(lamdel) is a single input variant using the n-by-2 array instead of
+%   two vectors. This simplifies calls to some function-functions, like fminsearch
+%   and mhsample. The first column is lambdas and the second column is deltas.
 
-narginchk(2,2)
+narginchk(1,2)
+if nargin == 1
+    validateattributes(lams,{'numeric'},{'2d','ncols',2},1)
+    dels = lams(:,2);
+    lams = lams(:,1);
+else
+    validateattributes(lams,{'numeric'},{'vector'},1)
+    validateattributes(dels,{'numeric'},{'vector'},2)
+    assert(length(lams) == length(dels), 'length(lams) ~= length(dels)')
+end
 
 cmp = CMSPlanet(length(lams));
 cmp.cms.lambdas = lams;
