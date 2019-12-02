08bd44ce5e9de6693be3c22d400cbecadf498905
Naor Movshovitz <nmovshov@gmail.com>
2019-12-02 15:10:57 -0800

    Generator for polynomial density profiles.

---
+++ +generators/polynomial.m
@@ -0,0 +1,47 @@
+function cmp = polynomial(N, x, zstrat, forcemono)
+%POLYNOMIAL A single polynomial in normalized equatorial radius.
+%    POLYNOMIAL(N, x) returns an N-point CMSPlanet object with rhoi approximated
+%    by a single polynomial of z=a/a0, with coefficients x. The default level
+%    spacing is one of equal radius increments between a/a0=1 and a/a0=1/N.
+%    
+%    POLYNOMIAL(N, x, zstrat) lets you specify the zvec distribution strategy.
+%    Pass a handle to a function that takes a single scalar integer (number of
+%    layers) and returns a vector of that length with values in the interval (0,
+%    1], for normalized equatorial layer radii.
+%
+%    POLYNOMIAL(...,forcemono) where forcemono==true forces the resulting density
+%    profile to be monotonically nonincreasing. Default forcemono is false.
+
+if nargin == 0
+    fprintf('Usage:\n\tcmp = polynomial(N, x, zstrat, forcemono)\n')
+    return
+end
+narginchk(2,4)
+if nargin < 3 || isempty(zstrat), zstrat = @(n)linspace(1, 1/n, n); end
+if nargin < 4 || isempty(forcemono), forcemono = false; end
+validateattributes(N, {'numeric'},{'positive','integer'},'','N',1)
+validateattributes(x, {'numeric'},{'real','finite','vector'},'','x',2)
+validateattributes(zstrat, {'function_handle'},{},'','zstrat',3)
+validateattributes(forcemono,{'logical'},{'scalar'},'','forcemono',4)
+
+cmp = CMSPlanet();
+
+zvec = zstrat(N);
+assert(isnumeric(zvec) && isvector(zvec) && (numel(zvec) == N),...
+    '@zstrat(N) must return a vector of length N with values in (0,1].')
+assert(all(zvec > 0) && all(zvec <= 1),...
+    '@zstrat(N) must return a vector of length N with values in (0,1].')
+
+dvec = polyval(x,zvec);
+
+if forcemono
+    dvec(1) = max(dvec(1), 0);
+    for k=2:N
+        dvec(k) = max(dvec(k), dvec(k-1));
+    end
+end
+
+cmp.ai = zvec;
+cmp.rhoi = dvec;
+
+end
